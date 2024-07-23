623bc919a896f1eea9b076a46f7cbff5c2311266
Gui-RB <guihermerochabritox@gmail.com>
2024-07-23 10:59:25 -0300

    first commit

---
+++ src/main/java/desafio/junior/autoscore/AutoscoreApplication.java
@@ -0,0 +1,13 @@
+package desafio.junior.autoscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoscoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/desafio/junior/autoscore/AutoscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.junior.autoscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
